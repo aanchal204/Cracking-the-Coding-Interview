71434f9a825fb10853605064f5e4d7241a93d5ef
Aanchal Dalmia <aanchal204@yahoo.in>
2016-07-23 00:24:48 +0530

    Chapter 1 Ques 1

---
+++ q1.cpp
@@ -0,0 +1,25 @@
+/*
+	Q1: Is Unique: Implement an algorithm to determine if a string has all unique characters. 
+*/
+#include<string>
+#include<iostream>
+using namespace std;
+#define SIZE 128
+bool hasUnique(string s){
+	bool arr[SIZE] = {0};
+	for(int i=0;i<s.length();i++){
+		if(arr[s[i]])
+			return false;
+		arr[s[i]] = true;
+	}
+	return true;
+}
+int main(void){
+	string s;
+	getline(cin,s);	//input a string with spaces
+	if(hasUnique(s))
+		cout<<"Has unique characters"<<endl;
+	else
+		cout<<"Doesnt have unique characters"<<endl;
+	return 0;
+}
