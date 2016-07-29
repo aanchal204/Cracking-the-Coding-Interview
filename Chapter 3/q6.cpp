84fe81bd21fd0085ce3545e0ed2032fb59b83bca
Aanchal Dalmia <aanchal204@yahoo.in>
2016-07-30 02:59:08 +0530

    Modified Ques 6

--- Chapter 3/q6.cpp
+++ Chapter 3/q6.cpp
@@ -1,8 +1,10 @@
-/*Animal Shelter: An animal shelter, which holds only dogs and cats, operates on astrcitly 'first in, first out' basis.
+/*
+Animal Shelter: An animal shelter, which holds only dogs and cats, operates on astrcitly 'first in, first out' basis.
 People must adopt either the oldest of all animals at the shelter, or they can select whether they would
 prefer a dog or a cat(and would receive the oldest of that type).They cannot select which specific animal
 they would like. Create a data structure to maintain this system and implement operations such as 
-enQueue, deQueueAny, deQueueDog and deQueueCat. You may use the built-in LinkedList data structure. */
+enQueue, deQueueAny, deQueueDog and deQueueCat. You may use the built-in LinkedList data structure. 
+*/
 
 #include<list>
 #include<iostream>
